056aa5e904c1d93c525e20f83f44dbecdd995ef0
deasequana-ux <yucehilal99@gmail.com>
2022-06-14 14:40:13 +0300

    add category tables

---
+++ ecommerce-finalproject/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using ecommerce_finalproject.Data;
+using ecommerce_finalproject.Data.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ecommerce_finalproject.Models
+{
+    public class Category:IEntityBase
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Category Name")]
+        public string CategoryName { get; set; }
+
+    }
+}
---
+++ ecommerce-finalproject/Models/CategoryProduct.cs
@@ -0,0 +1,27 @@
+﻿using ecommerce_finalproject.Data;
+using ecommerce_finalproject.Data.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ecommerce_finalproject.Models
+{
+    public class CategoryProduct:IEntityBase
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+
+        public int ProductId { get; set; }
+        [ForeignKey("ProductId")]
+
+        [Display(Name = "Category Name")]
+        public Category categories { get; set; }
+
+    }
+}
